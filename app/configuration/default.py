fa07e854765c9b1a39efb3a660f6467d1c0efca7
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2023-12-28 11:30:22 +0100

    iter

--- app/configuration/default.py
+++ app/configuration/default.py
@@ -1,8 +1,8 @@
 # Retriever parameters
-SEMANTIC_RETRIEVER_PATH = "../models/user_guide_semantic_retrieval.joblib"
-SEMANTIC_TOP_K = 5
-LEXICAL_RETRIEVER_PATH = "../models/user_guide_lexical_retrieval.joblib"
-LEXICAL_TOP_K = 5
+API_SEMANTIC_RETRIEVER_PATH = "../models/user_guide_semantic_retrieval.joblib"
+API_SEMANTIC_TOP_K = 5
+API_LEXICAL_RETRIEVER_PATH = "../models/user_guide_lexical_retrieval.joblib"
+API_LEXICAL_TOP_K = 5
 CROSS_ENCODER_PATH = "cross-encoder/ms-marco-MiniLM-L-6-v2"
 CROSS_ENCODER_THRESHOLD = 2.0
 CROSS_ENCODER_MIN_TOP_K = 5
